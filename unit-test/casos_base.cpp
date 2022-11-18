c64a70ccca5f1519eed5884add6b59633a85a4ab
Paula Pecker <paulapeckerfasce@gmail.com>
2022-11-18 19:58:10 -0300

    ponele que unit test

--- unit-test/casos_base.cpp
+++ unit-test/casos_base.cpp
@@ -1,15 +1,41 @@
 #include "gmock/gmock.h"
+#include "FUNCIONES.h"
+namespace Casos_Base::tests 
+{
+	TEST(convertDateToTimeT, C1)
+	{
+		tm day;//la inicializo en tm parra pasarla a time_t y es string para verificar que me den lo mismo
+		day.tm_year = 2022;
+		day.tm_mon = 11; //dic
+		day.tm_mday = 18; 
 
-namespace Casos_Base::tests {
-	TEST(Casos_Base, Test_Caso1) {
-		ASSERT_THAT(1, 1);
+		string daySTR = "18/11/2022";
+
+		time_t seg = mktime(&day);
+
+		EXPECT_THAT(convertDateToTimeT(daySTR), seg);
+
+	}
+	
+	
+	TEST(compareDates, normal) //normalito
+	{
+	
+		time_t todayt = time(0);
+
+		string sod = "12/11/2022";
+		time_t sodT = convertDateToTimeT(sod);
+
+		EXPECT_THAT(compareDates(todayt, sodT), 1);
 	}
 
-	TEST(Casos_Base, Test_Caso2) {
-		ASSERT_THAT(1, 1);
-	}
+	TEST(compareDates, sameday)
+	{
+		string sameD = "12/11/2006";
+		time_t sameDT = convertDateToTimeT(sameD);
 
-	TEST(Casos_Base, Test_Caso3) {
-		ASSERT_THAT(1, 1);
+		EXPECT_THAT(compareDates(sameDT, sameDT), 2);
+
 	}
+	
 }
